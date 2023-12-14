2e97c0d53e17365c08b7b1da791f4cbb39caee6e
humphreyotieno1 <humphreyotieno04@gmail.com>
2023-12-15 00:19:37 +0300

    More callback functions

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers) {
+    return drivers.slice(0, 2);
+}
+
+const returnLastTwoDrivers = function(drivers) {
+    return drivers.slice(-2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const dArray = ['Antonia', 'Nuru', 'Hum', 'Nobert'];
+const myFirstDrivers = selectingDrivers[0](dArray);
+const myLastDrivers = selectingDrivers[1](dArray);
+console.log(myFirstDrivers);
+console.log(myLastDrivers);
+
+const createFareMultiplier = function(multiplier) {
+    return function(fare) {
+        return fare * multiplier;
+    }
+}
+
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = function(drivers, selectingFunction) {
+    return selectingFunction(drivers);
+}
